98cf8f1ea5f00bfd369d4a4bae04ba5cd205ef56
Mateus Braga <teteu040506@gmail.com>
2024-02-17 23:10:05 -0300

    chore: CRACO config

---
+++ poc-mfe-capacitor-host/craco.config.js
@@ -0,0 +1,37 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  webpack: {
+    configure: {
+      output: {
+        publicPath: 'auto',
+      },
+    },
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: 'host',
+          remotes: {
+            remote: 'remote@http://localhost:3002/remoteEntry.js',
+          },
+          exposes: {},
+          filename: 'remoteEntry.js',
+          shared: {
+            ...deps,
+            react: {
+              singleton: true,
+              eager: true,
+              requiredVersion: deps['react'],
+            },
+            'react-dom': {
+              singleton: true,
+              eager: true,
+              requiredVersion: deps['react-dom'],
+            },
+          },
+        }),
+      ],
+    },
+  },
+};
